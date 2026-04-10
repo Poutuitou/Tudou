e6f893bcec64f35521ae7daf6625a50f7ffa4bfa
Poutuitou <2686025131@qq.com>
2026-04-10 16:08:23 +0800

    更新土豆文案

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('.action-button');
     if (button) {
         button.addEventListener('click', () => {
-            alert('我是土豆，千变万化，你想什么样，我就是什么样');
+            alert('我是土豆，是马铃薯，是洋芋粑，是薯条...我千变万化，我无处不在，你想什么样，我是什么样');
         });
     }
 });
